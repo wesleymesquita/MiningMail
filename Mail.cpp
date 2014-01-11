6b0db677cfd5411b73d61ddd088f7b75879e58b3
Wesley Mesquita <wesleymesquita@gmail.com>
2014-01-11 13:23:17 -0200

    Added features in Mail class

--- Mail.cpp
+++ Mail.cpp
@@ -1,23 +1,44 @@
 #include "Mail.h"
 
-Mail::Mail(const std::string& fileLoc)
-{
-   std::ofstream mailFile(fileLoc, std::ios::in);
-   
-   if( !mailFile.is_open() ){
-       throw std::exception();
-   }
-   else{
-       if(mailFile.good()){
-           std::stringstream s;
-           s << mailFile.rdbuf();
-           mailData = s.str();
-       }
-       mailFile.close();
-   }
-}   
+Mail::Mail(const std::string& fileLoc) {
+    loadMailRawdata(fileLoc);
+}
 
-const std::string& Mail::getRawMailData() const
-{
-    return this->mailData;
+const std::string& Mail::getFrom() const {
+    return this->from;
+}
+
+const std::vector<std::string>& Mail::getTo() const {
+    return this->to;
+}
+
+const boost::gregorian::date& Mail::getDate() const {
+    return this->date;
+}
+
+const std::string& Mail::getSubject() const {
+    return this->subject;
+}
+
+const std::string& Mail::getMailData() const {
+    return this->rawData;
+}
+
+void Mail::loadMailRawdata(const std::string& fileLoc) {
+    std::ofstream mailFile(fileLoc, std::ios::in);
+
+    if (!mailFile.is_open()) {
+        throw std::exception();
+    } else {
+        if (mailFile.good()) {
+            std::stringstream s;
+            s << mailFile.rdbuf();
+            rawData = s.str();
+        }
+        mailFile.close();
+    }
+}
+
+void Mail::parseMailData() {
+
 }
--- Mail.h
+++ Mail.h
@@ -14,15 +14,38 @@
 #include<algorithm>
 #include<iterator>
 
+#include<boost/date_time/gregorian/gregorian.hpp>
+
 #include <exception>
 
-class Mail
-{
+class Mail {
 public:
-   Mail(const std::string& fileLoc);     
-   const std::string& getRawMailData() const;
+    Mail(const std::string& fileLoc);
+    const std::string& getFrom() const;
+    const std::vector<std::string>& getTo() const;
+    const boost::gregorian::date& getDate() const;
+    const std::string& getSubject() const;
+    const std::string& getMailData() const;
+
 private:
-    std::string mailData; 
+
+    std::string from;
+    std::vector<std::string> to;
+    boost::gregorian::date date;
+    std::string subject;
+    std::string rawData;
+
+
+    std::string xFrom;
+    std::vector<std::string> xTo;
+    std::vector<std::string> xCC;
+    std::vector<std::string> xBCC;
+    std::string xFolder;
+    std::string xOrigin;
+    std::string xFileName;
+
+    void loadMailRawdata(const std::string& fileLoc);
+    void parseMailData();
 };
 
 #endif	/* ENRONMAIL_H */

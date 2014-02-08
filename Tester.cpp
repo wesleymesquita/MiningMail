285877c8ae819b9f13b6fcfca0a4bc3458bc714e
Wesley Mesquita <wesleymesquita@gmail.com>
2014-02-08 12:18:06 -0200

    Factoring to Tester helper class

--- Tester.cpp
+++ Tester.cpp
@@ -1,16 +1,59 @@
 #include "Tester.h"
 namespace mm {
-
-    bool Tester::assertStrings(
-            const char* function_name,
-            const char* expected,
-            const char* result) {
-        if (std::string(expected).compare(result) != 0) {
-            LOG_MESSAGE(className.c_str(), "[FAILED] getSubject failed");
-            return false;
-        } else {
-            LOG_MESSAGE(className.c_str(), "");
-            return true;
-        }
+    Tester::Tester(const char* _className): 
+        className(_className){        
     }
+    void Tester::addTestFunction(const std::string& funcName, 
+                                std::function<bool()> test_fn){
+        funcs.push_back(funcName);
+        test_functions[funcName] = std::move(test_fn);
+        results[funcName] = false;      
+    }
+    
+    bool Tester::execTest(){
+        std::for_each(funcs.begin(), funcs.end(), 
+                [&](const std::string& func){
+                    results[func] = test_functions[func]();
+                });
+    }
+    
+    void Tester::cleanTestResults(){
+        std::for_each(funcs.begin(), funcs.end(), 
+                [&](const std::string& func){
+                    results[func] = false;
+                });
+    }
+    
+    bool Tester::getFinalResult() const{
+        bool res = true;
+        for(auto& f: funcs){
+            res &= results.at(f);
+        }
+        return res;
+    }
+    
+    std::string Tester::getDetailResult(){
+       static const std::string FALSE("false");
+       static const std::string TRUE("true");
+              
+       namespace bptree = boost::property_tree;
+       bptree::ptree pt;
+       
+       pt.put("className", className);
+       
+       bptree::ptree func_nodes;
+       for(std::string& func: funcs){
+           bptree::ptree func_item;
+           func_item.put(func, [&](){return results[func] ? TRUE : FALSE; }());
+           func_nodes.push_back(std::make_pair("", func_item));
+       }
+       pt.add_child("test_func_results", func_nodes);
+       pt.put("all_passed", [&](){return  getFinalResult() ? TRUE : FALSE; }());
+    
+       std::stringstream sstr;
+       bptree:write_json(sstr, pt, false);
+       return std::move(sstr.str());
+    }
+    
+    
 };
--- Tester.h
+++ Tester.h
@@ -1,29 +1,73 @@
 /* 
  * File:   Tester.h
- * Author: Wesley Mesquita
- *
- * Created on 19 de Janeiro de 2014, 19:35
+ * @Author: Wesley Mesquita
+ * @email: wesleymesquita@gmail.com
+ * 
+ * Created Jan 19, 2014
+ * Updated: Fev 08, 2014
  */
 
 #ifndef TESTER_H
 #define	TESTER_H
 
 #include<string>
-#include<boost/current_function.hpp>
+#include<functional>
+#include<unordered_map>
+#include<utility>
+#include<algorithm>
+
+#include<boost/property_tree/ptree.hpp>
 
 #include "MiningMail.h"
-namespace mm {
 
+namespace mm {
+    /** @brief Tester is a framework to handle simple testing tasks
+     *  A test class must inherits from Tester and implement
+     *  test function that fits in std::function<bool()>
+     *  
+     * 
+     */
     class Tester {
     public:
+        /**
+         * @note className is just a identifier for a set of tests, 
+         *        could not be a c++ class name.   
+         */
         Tester(const char* className);
-        bool assertStrings(const char* funtion_name,
-                const char* expected,
-                const char* result);
+        
+        /**
+         * @param funcName Function name that is tested
+         * 
+         */
+        void addTestFunction(const std::string& funcName, std::function<bool()>);
+        
+        /**
+         *  @brief runs all test functions added
+         */
+        bool execTest();
+        
+        /**
+         *  @brief cleans result vector
+         */
+        void cleanTestResults();        
+        
+        /**
+         *  @brief gets a JSON string with test results: the final result and 
+         *         each function (in case of a fail, the user can identify where
+         *         it happened)        
+         */
+        std::string getDetailResult();
+        
+        
+        /**
+         * @brief get the overall result
+         */
+        bool getFinalResult() const;
     private:
         std::string className;
-        const char* FAILED = "FAILED";
-
+        std::vector<std::string> funcs;
+        std::unordered_map<std::string, std::function<bool()>> test_functions;
+        std::unordered_map<std::string, bool> results;
     };
 };
 
